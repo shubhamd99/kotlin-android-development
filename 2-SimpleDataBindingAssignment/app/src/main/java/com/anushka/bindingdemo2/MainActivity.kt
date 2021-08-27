3e34384b8b2b36e8755a29801f5a8a77e9525d23
Shubham Dhage <shubham.dhage@swiggy.in>
2021-08-27 15:05:22 +0530

    simple data binding assignment

--- 2-SimpleDataBindingAssignment/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
+++ 2-SimpleDataBindingAssignment/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
@@ -3,29 +3,33 @@ package com.anushka.bindingdemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
+import com.anushka.bindingdemo2.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.control_button)
-        button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // control_button -> controlButton
+        binding.controlButton.setOnClickListener {
             startOrStopProgressBar()
         }
     }
 
     private fun startOrStopProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        if (progressBar.visibility == View.GONE) {
-            progressBar.visibility = View.VISIBLE
-            button.text = "Stop"
-        } else {
-            progressBar.visibility = View.GONE
-            button.text = "Start"
+        binding.apply {
+            if (progressBar.visibility == View.GONE) {
+                progressBar.visibility = View.VISIBLE
+                controlButton.text = "Stop"
+            } else {
+                progressBar.visibility = View.GONE
+                controlButton.text = "Start"
+            }
         }
+
     }
 }
 
